cf3c51348bc4d9ada940be83aa97b9ca3daead46
roh-7 <rohit.6233@outlook.com>
2017-02-25 15:28:24 +0530

    BottomBAr added

---
+++ app/src/main/java/com/example/virtuallibrarian/activities/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.example.virtuallibrarian.activities;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.virtuallibrarian.R;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabReselectListener;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        BottomBar bottomBar = (BottomBar)findViewById(R.id.bottomBar);
+        bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
+            @Override
+            public void onTabReSelected(@IdRes int tabId) {
+                
+            }
+        });
+    }
+}
