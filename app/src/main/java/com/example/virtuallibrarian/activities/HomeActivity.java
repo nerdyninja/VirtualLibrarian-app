56a85243f696bcaad3d917677f6fc9a1cddba082
roh-7 <rohit.6233@outlook.com>
2017-02-25 19:07:06 +0530

    Profile switching

--- app/src/main/java/com/example/virtuallibrarian/activities/HomeActivity.java
+++ app/src/main/java/com/example/virtuallibrarian/activities/HomeActivity.java
@@ -2,12 +2,15 @@ package com.example.virtuallibrarian.activities;
 
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.example.virtuallibrarian.R;
+import com.example.virtuallibrarian.fragments.ProfileFragment;
 import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnTabReselectListener;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -17,10 +20,20 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Log.v("t","t");
         BottomBar bottomBar = (BottomBar)findViewById(R.id.bottomBar);
-        bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
-            public void onTabReSelected(@IdRes int tabId) {
+            public void onTabSelected(@IdRes int tabId) {
+                Fragment fragment = null;
+                if(tabId==R.id.fourth)
+                {
+                    fragment = new ProfileFragment();
+                }
 
+                if (fragment != null) {
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.fragment_container, fragment);
+                    ft.commit();
+                }
             }
         });
     }
--- app/src/main/java/com/example/virtuallibrarian/fragments/BooksFragment.java
+++ app/src/main/java/com/example/virtuallibrarian/fragments/BooksFragment.java
@@ -1,8 +1,8 @@
 package com.example.virtuallibrarian.fragments;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,15 +13,15 @@ import com.example.virtuallibrarian.R;
  * Created by rohitramaswamy on 25/02/17.
  */
 
-public class ProfileFragment extends Fragment {
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
+public class BooksFragment extends Fragment {
 
+    public BooksFragment()
+    {}
+
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_profile, container, false);
-        Log.v("t","o");
-        return view;
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
