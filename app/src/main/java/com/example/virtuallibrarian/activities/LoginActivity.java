225321cfa7526dc65cd142fe0bf579981390cf46
roh-7 <rohit.6233@outlook.com>
2017-02-25 13:44:10 +0530

    Login design ready

---
+++ app/src/main/java/com/example/virtuallibrarian/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.virtuallibrarian.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.virtuallibrarian.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
